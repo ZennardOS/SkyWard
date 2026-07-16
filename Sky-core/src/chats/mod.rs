7b8931c48d0a01ac8a851d0fa8b62a52f55a28db
ZennardOS <shadowkod2006@gmail.com>
2026-07-16 09:44:03 +0300

    [CHATS]: created get_chat func

--- Sky-core/src/chats/mod.rs
+++ Sky-core/src/chats/mod.rs
@@ -21,7 +21,7 @@ pub async fn get_chat(pool: &SqlitePool, my_account: &Account, contact: &Contact
     let created_date = Utc::now().to_rfc3339();
 
     sqlx::query(r#"
-        INSERT INTO Chat (
+        INSERT INTO chats (
             chat_id,
             account_id,
             contact_id,
@@ -34,5 +34,58 @@ pub async fn get_chat(pool: &SqlitePool, my_account: &Account, contact: &Contact
 "#,
     ).bind(&chat_id).bind(&my_account.account_id).bind(&contact.contact_id).bind(&contact.peer_account_id).bind(&created_date).execute(pool).await?;
 
-    todo!("WILL DO");
+    let line = sqlx::query_as::<_, (String, String, String, String, String, Option<String>)>(
+        r#"
+        SELECT
+            chat_id,
+            account_id,
+            contact_id,
+            peer_account_id,
+            created_date,
+            last_message_date
+        FROM chats
+        WHERE account_id = ? AND peer_account_id = ?
+"#,
+    ).bind(&my_account.account_id).bind(&contact.peer_account_id).fetch_one(pool).await?;
+
+    return Ok(Chat {
+        chat_id: line.0,
+        account_id: line.1,
+        contact_id: line.2,
+        peer_account_id: line.3,
+        created_date: line.4,
+        last_message_date: line.5,
+    });
+
+}
+
+
+pub async fn list_chats(pool: &SqlitePool, my_account: &Account) -> Result<Vec<Chat>> {
+    let lines = sqlx::query_as::<_, (String, String, String, String, String, Option<String>)>(
+        r#"
+            SELECT
+                chat_id,
+                account_id,
+                contact_id,
+                peer_account_id,
+                created_date,
+                last_message_date
+            FROM chats
+            WHERE account_id = ?
+            ORDER BY COALESCE(last_message_date, created_date) DESC
+"#,
+    ).bind(&my_account.account_id).fetch_all(pool).await?;
+
+    let chats: Vec<Chat> = lines.into_iter().map(|line| Chat {
+        chat_id: line.0,
+        account_id: line.1,
+        contact_id: line.2,
+        peer_account_id: line.3,
+        created_date: line.4,
+        last_message_date: line.5,
+    })
+                                            .collect();
+
+    Ok(chats)
+
 }
